216d3e311badb08578c3d96a6a62662e0eb2ca0a
AdieDev0 <devutella@gmail.com>
2025-02-08 23:28:44 +0800

    style: update Navbar component MenuButton for improved styling and readability

--- jollibee/src/components/Navbar.jsx
+++ jollibee/src/components/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
           </Menu>
           <Menu as="div" className="relative inline-block text-left">
             <div>
-              <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50">
+              <MenuButton className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 font-bold text-xl text-white">
                 Promotion
                 <FaAngleDown
                   aria-hidden="true"
