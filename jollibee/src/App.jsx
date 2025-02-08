5b56265ab38ebc08797b6246330302d7256bb887
AdieDev0 <devutella@gmail.com>
2025-02-08 19:30:51 +0800

    add routing and Home component to App

--- jollibee/src/App.jsx
+++ jollibee/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import { Router, Routes, Route } from "react-router-dom";
+import Delivery from "./components/Delivery";
+import Location from "./components/Location";
+import ViewMenu from "./components/ViewMenu";
+import MultiDelivery from "./components/MultiDelivery";
+import Promos from "./components/Promos";
+import GiftCard from "./components/GiftCard";
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/delivery" element={<Delivery />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
