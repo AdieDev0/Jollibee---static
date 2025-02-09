a1eb221dcabdad80001a6c3158ecdedd7621af41
AdieDev0 <devutella@gmail.com>
2025-02-10 01:44:36 +0800

    feat: enhance Home component with banner and call-to-action for improved user experience

--- jollibee/src/components/Home.jsx
+++ jollibee/src/components/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-
+import React from "react";
+import HomeBanner from "../assets/2021/HomeBanner.png";
 const Home = () => {
   return (
     <div>
-      
+      <div>
+        <img src={HomeBanner} alt="Home Banner" className="w-full relative"/>
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
+          <h1>Bringing joy to you</h1>
+          <h2>
+            Have your Jollibee favorites delivered right to your doorstep!
+          </h2>
+          <button>Order Now</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
