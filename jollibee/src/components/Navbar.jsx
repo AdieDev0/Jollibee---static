26367a2ab49ad2764224c274558c80194a19c11a
AdieDev0 <devutella@gmail.com>
2025-02-09 21:52:37 +0800

    feat: update Navbar component layout and enhance dropdown menu functionality

--- jollibee/src/components/Navbar.jsx
+++ jollibee/src/components/Navbar.jsx
@@ -3,120 +3,117 @@ import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 import { FaAngleDown } from "react-icons/fa6";
 const Navbar = () => {
   return (
-    <div>
-      <div className="bg-[#D6001C]">
-        <ul className="flex items-center gap-4">
-          <li className="text-xl font-bold text-white">Delivery</li>
-          <li className="text-xl font-bold text-white">Location</li>
-          <Menu as="div" className="relative inline-block text-left">
-            <div className="items-center">
-              <MenuButton className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 font-bold text-xl text-white">
-                Menu
-                <FaAngleDown
-                  aria-hidden="true"
-                  className="size-5 text-white"
-                />
-              </MenuButton>
-            </div>
+    <div className="bg-[#D6001C]">
+      <ul className="flex justify-end items-center gap-4">
+        <li className="text-xl font-bold text-white">Delivery</li>
+        <li className="text-xl font-bold text-white">Location</li>
+        <Menu as="div" className="relative inline-block text-left">
+          <div className="items-center">
+            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 font-bold text-xl text-white">
+              Menu
+              <FaAngleDown aria-hidden="true" className="size-5 text-white" />
+            </MenuButton>
+          </div>
 
-            <MenuItems
-              transition
-              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-            >
-              <div className="py-1">
+          <MenuItems
+            transition
+            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+          >
+            <div className="py-1">
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                >
+                  Account settings
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                >
+                  Support
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                >
+                  License
+                </a>
+              </MenuItem>
+              <form action="#" method="POST">
                 <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                  <button
+                    type="submit"
+                    className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
                   >
-                    Account settings
-                  </a>
+                    Sign out
+                  </button>
                 </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                  >
-                    Support
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                  >
-                    License
-                  </a>
-                </MenuItem>
-                <form action="#" method="POST">
-                  <MenuItem>
-                    <button
-                      type="submit"
-                      className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                    >
-                      Sign out
-                    </button>
-                  </MenuItem>
-                </form>
-              </div>
-            </MenuItems>
-          </Menu>
-          <Menu as="div" className="relative inline-block text-left">
-            <div>
-              <MenuButton className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 font-bold text-xl text-white">
-                Promotion
-                <FaAngleDown
-                  aria-hidden="true"
-                  className="-mr-1 size-5 text-white"
-                />
-              </MenuButton>
+              </form>
             </div>
+          </MenuItems>
+        </Menu>
+        <Menu as="div" className="relative inline-block text-left">
+          <div>
+            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 font-bold text-xl text-white">
+              Promotion
+              <FaAngleDown
+                aria-hidden="true"
+                className="-mr-1 size-5 text-white"
+              />
+            </MenuButton>
+          </div>
 
-            <MenuItems
-              transition
-              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
-            >
-              <div className="py-1">
+          <MenuItems
+            transition
+            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+          >
+            <div className="py-1">
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                >
+                  Account settings
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                >
+                  Support
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                >
+                  License
+                </a>
+              </MenuItem>
+              <form action="#" method="POST">
                 <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                  <button
+                    type="submit"
+                    className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
                   >
-                    Account settings
-                  </a>
+                    Sign out
+                  </button>
                 </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                  >
-                    Support
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                  >
-                    License
-                  </a>
-                </MenuItem>
-                <form action="#" method="POST">
-                  <MenuItem>
-                    <button
-                      type="submit"
-                      className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
-                    >
-                      Sign out
-                    </button>
-                  </MenuItem>
-                </form>
-              </div>
-            </MenuItems>
-          </Menu>
-          <button>Order Now</button>
-        </ul>
-      </div>
+              </form>
+            </div>
+          </MenuItems>
+        </Menu>
+        <button className="bg-orange-400 text-white font-bold text-xl rounded-full px-5 py-3 shadow-lg">
+          Order Now
+        </button>
+      </ul>
     </div>
   );
 };
