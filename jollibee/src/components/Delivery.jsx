2f7c56a34eb3fff736af2f1256156966aeb20339
AdieDev0 <devutella@gmail.com>
2025-02-16 12:15:24 +0800

    feat: add accordion component to Delivery for improved content organization

--- jollibee/src/components/Delivery.jsx
+++ jollibee/src/components/Delivery.jsx
@@ -109,6 +109,34 @@ const Delivery = () => {
           </div>
         </div>
       </div>
+
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" defaultChecked />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
+      <div className="collapse collapse-plus bg-base-200">
+        <input type="radio" name="my-accordion-3" />
+        <div className="collapse-title text-xl font-medium">
+          Click to open this one and close others
+        </div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
     </>
   );
 };
