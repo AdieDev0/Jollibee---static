4da7cdb68ce67a0017cab7a825b70d90ba11534b
AdieDev0 <devutella@gmail.com>
2025-02-19 21:38:58 +0800

    style: update title in Burger component to reflect menu category

--- jollibee/src/components/Burger.jsx
+++ jollibee/src/components/Burger.jsx
@@ -25,7 +25,7 @@ const Burger = () => {
       {/* Main Content */}
       <div className="flex-1 p-6 md:p-12">
         <p className="text-center font-extrabold text-4xl font-oswald text-black/80 mb-12">
-          Family Meals
+          Burgers
         </p>
 
         {/* Responsive Grid */}
