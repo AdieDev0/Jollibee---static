c28cde71581b2d773c19e96ef893a1150c6bdfa2
AdieDev0 <devutella@gmail.com>
2025-02-19 20:43:37 +0800

    style: add Burgers item to SidebarMenu component

--- jollibee/src/components/SidebarMenu.jsx
+++ jollibee/src/components/SidebarMenu.jsx
@@ -124,7 +124,26 @@ const SidebarMenu = () => {
               <span className="ml-5 font-bold">Chickenjoy</span>
             </NavLink>
           </li>
-          {/*  */}
+          {/* Burgers */}
+          <li>
+            <NavLink
+              to="/Burger"
+              className={({ isActive }) =>
+                `flex items-center p-3 font-semibold text-lg rounded-lg transition-all duration-200 transform ${
+                  isActive
+                    ? "bg-white text-black scale-105 shadow-md border-l-8 border-yellow-500"
+                    : "text-black/80 hover:bg-white hover:scale-105"
+                }`
+              }
+            >
+              <img
+                src={Burger}
+                alt="New Products"
+                className="w-16 h-16 object-cover rounded-lg shadow-sm"
+              />
+              <span className="ml-5 font-bold">Burgers</span>
+            </NavLink>
+          </li>
         </ul>
       </div>
     </aside>
