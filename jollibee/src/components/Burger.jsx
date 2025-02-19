41a0dbda5170238bdea62fbd2cea123e09bfc37d
AdieDev0 <devutella@gmail.com>
2025-02-19 21:43:36 +0800

    style: add new menu items to Burger component with descriptions and images

--- jollibee/src/components/Burger.jsx
+++ jollibee/src/components/Burger.jsx
@@ -6,11 +6,42 @@ import BaconCheesyYumburgerSolo from "../assets/Menu/Burger/BaconCheesyYumburger
 import ChampJrSolo from "../assets/Menu/Burger/ChampJrSolo.png";
 import ChampOne from "../assets/Menu/Burger/ChampOne.webp";
 import CheesyYumburgerSolo from "../assets/Menu/Burger/CheesyYumburgerSolo.png";
+import AmazaingAlohaChampJr from "../assets/Menu/Burger/AmazingAlohaChampJr.png";
 const menuItems = [
   {
     name: "Yumburger",
     image: YumburgerSolo,
     desc: "Your favorite beefiest langhap-sarap Yumburger, with beefy patty and our special dressing in between soft buns.",
+  },
+  {
+    name: "Cheesy Yumburger",
+    image: CheesyYumburgerSolo,
+    desc: "Your favorite beefiest langhap-sarap Yumburger with creamy cheese.",
+  },
+  {
+    name: "Bacon Cheesy Yumburger",
+    image: BaconCheesyYumburgerSolo,
+    desc: "Your favorite langhap-sarap Yumburger now made extra special with crispy bacon and creamy cheese.",
+  },
+  {
+    name: "Champ Jr.",
+    image: ChampJrSolo,
+    desc: "The Classic Champ now in a Jr. size. Made with a beefy patty, lettuce, tomato, and creamy cheese.",
+  },
+  {
+    name: "Champ",
+    image: ChampOne,
+    desc: "The iconic 1/3-pound Champ patty with tomato, lettuce and cheese in sesame seed buns.",
+  },
+  {
+    name: "Aloha Champ Jr.",
+    image: AmazaingAlohaChampJr,
+    desc: "The Aloha Champ now has a Jr. size. Made with a beefy patty, juicy pineapple, crispy bacon, honey mustard dressing, and creamy cheese.",
+  },
+  {
+    name: "Aloha Champ",
+    image: AmazingAlohaChamp,
+    desc: "The iconic 1/3-pound Champ patty with juicy pineapple rings, crispy bacon strips, lettuce, cheese and honey mustard dressing in sesame seed buns.",
   },
 ];
 
