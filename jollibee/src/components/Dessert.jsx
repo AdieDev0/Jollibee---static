8d4dcb8ddceb2831899e2c717f9a701a98538c2c
AdieDev0 <devutella@gmail.com>
2025-02-21 12:47:11 +0800

    style: update title in Dessert component from "Super Meals" to "Desserts"

--- jollibee/src/components/Dessert.jsx
+++ jollibee/src/components/Dessert.jsx
@@ -38,7 +38,7 @@ const Dessert = () => {
       {/* Main Content */}
       <div className="flex-1 p-6 md:p-12">
         <p className="text-center font-extrabold text-4xl font-oswald text-black/80 mb-12">
-          Super Meals
+          Desserts
         </p>
 
         {/* Responsive Grid */}
