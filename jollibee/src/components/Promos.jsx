ffc4b99f644ec501ade9b3dee499984150dd38da
AdieDev0 <devutella@gmail.com>
2025-02-23 20:22:34 +0800

    style: update Promos component with enhanced styling and layout

--- jollibee/src/components/Promos.jsx
+++ jollibee/src/components/Promos.jsx
@@ -4,8 +4,12 @@ const Promos = () => {
   return (
     <div>
       {/* Promos */}
-      <div className='bg'>
-        Promos
+      <div className='bg-red-600 p-12'>
+        <p className='text-white font-bold text-5xl font-roboto text-center'>Promos</p>
+      </div>
+
+      <div className='bg-white'>
+
       </div>
     </div>
   )
