0253a0d417f3b74d91e21f04b98bb66821fcbf81
AdieDev0 <devutella@gmail.com>
2025-02-23 20:25:00 +0800

    style: enhance Promos component with improved layout and additional content

--- jollibee/src/components/Promos.jsx
+++ jollibee/src/components/Promos.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Promos = () => {
   return (
     <div>
       {/* Promos */}
-      <div className='bg-red-600 p-12'>
-        <p className='text-white font-bold text-5xl font-roboto text-center'>Promos</p>
+      <div className="bg-red-600 p-12">
+        <p className="text-white font-bold text-5xl font-roboto text-center">
+          Promos
+        </p>
       </div>
 
-      <div className='bg-white'>
+      <div className="bg-white p-10">
+        <h1 className="text-4xl font-roboto font-bold text-center text-red-600 mb-10">
+          Jollibee Promo
+        </h1>
+        <p className=" text-center text-black font-roboto">
+          Get more JOY with these exciting promos! Take advantage of these
+          exclusive and limited time only offers and deals to get big discounts
+          and freebies!
+        </p>
 
+
+        {/* Cards */}
+        <div>
+          
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Promos
+export default Promos;
