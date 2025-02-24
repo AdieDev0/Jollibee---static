bca8b9a546716488b36e07d022b60378572707ba
AdieDev0 <devutella@gmail.com>
2025-02-24 10:36:01 +0800

    style: enhance GiftCard component layout with improved padding and button styles

--- jollibee/src/components/GiftCard.jsx
+++ jollibee/src/components/GiftCard.jsx
@@ -10,9 +10,9 @@ const GiftCard = () => {
         </p>
       </div>
 
-      <div className="flex gap-10 items-center">
+      <div className="flex gap-10 items-center px-48 py-5">
         <img src={Egifts} alt="" />
-        <div className="grid gap-5">
+        <div className="grid gap-10 w-2xl">
           <p className="text-4xl font-bold font-roboto text-red-600">Jolly e-Gifts</p>
           <h1>
             A digital platform where customers can conveniently send electronic
@@ -21,9 +21,9 @@ const GiftCard = () => {
             redeemed at any Jollibee store. Send a joyful treat on any special
             occasion.
           </h1>
-          <div className="flex gap-5">
-            <button>Buy e-Gift Certificates</button>
-            <button>Buy e-Product Vouchers</button>
+          <div className="flex gap-16">
+            <button className="bg-orange-400 hover:bg-orange-500 duration-200 text-xl text-white font-bold font-roboto rounded-full p-3 cursor-pointer">Buy e-Gift Certificates</button>
+            <button className="border-2 border-red-500 hover:border-red-600 hover:text-red-600 duration-200 text-xl text-red-500 font-bold font-roboto rounded-full p-3 cursor-pointer">Buy e-Product Vouchers</button>
           </div>
         </div>
       </div>
